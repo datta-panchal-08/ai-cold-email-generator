a73d329e39eda700f4341caa23aef0d9ad03252c
Dattatray Panchal <dattatraypanchal.dev@gmail.com>
2026-07-18 06:47:57 -0700

    feat: initial release of AI Cold Email Generator

--- client/src/api/endpoint.js
+++ client/src/api/endpoint.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: "https://ai-cold-email-generator-rqy7.onrender.com/api/v1",
 });
 
 axiosInstance.interceptors.request.use((config) => {
--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 const _dirname = path.resolve();
 
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://ai-cold-email-generator-rqy7.onrender.com"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
 }));
